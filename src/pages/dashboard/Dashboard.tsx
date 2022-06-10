564c8ec6a70000cf84f7b752294aab2e21a9c620
Fábio Penha <fabiopenha95@outlook.com>
2022-06-10 11:48:17 -0300

    feat: renomeando a pasta ferramendas-da-listagem

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,15 @@
+import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 
-
 export const Dashboard = () => {
-  return <LayoutBaseDePagina titulo='Página Inicial' barraDeFerramentas={<>Barra de Ferramentas</>}>Testando</LayoutBaseDePagina>;
+  return (
+    <LayoutBaseDePagina
+      titulo="Página Inicial"
+      barraDeFerramentas={
+        <FerramentasDaListagem mostrarInputBusca textoBotaoNovo="Nova" />
+      }
+    >
+      Testando
+    </LayoutBaseDePagina>
+  );
 };
